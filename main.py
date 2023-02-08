7d5ae22defe8b619bc3abdd326712aa4ef5d29cf
SeregaLi <kaprall13rus@gmail.com>
2023-02-08 15:28:29 +0300

    Final

--- main.py
+++ main.py
@@ -5,14 +5,12 @@ import logging
 # Устанавливаем параметры запроса
 url = 'https://api.binance.com/api/v1/ticker/price'
 params = {'symbol': 'XRPUSDT'}
-threshold = 1 # Задаем пороговое значение для анализа цены
+threshold = 1  # Задаем пороговое значение для анализа цены
 
 # Получаем текущую цену и максимальную цену за последний час
 response = requests.get(url, params=params).json()
 current_price = float(response['price'])
 max_price = float(response['price'])
-
-
 
 # Запускаем программу в цикле
 while True:
@@ -22,10 +20,11 @@ while True:
         response = requests.get(url, params=params).json()
         current_price = float(response['price'])
         if (max_price - current_price) / max_price * 100 >= threshold:
-            logging.warning('Цена фьючерса XRP/USDT упала на 1% или более от максимальной цены за последний час!')
-            print('Цена фьючерса XRP/USDT упала на 1% или более от максимальной цены за последний час!')
+            logging.warning(
+                'Цена фьючерса XRP/USDT упала на 1% или более от максимальной цены за последний час!')
+            print(
+                'Цена фьючерса XRP/USDT упала на 1% или более от максимальной цены за последний час!')
     except:
         logging.error('Ошибка при получении данных')
     # Задержка перед следующим циклом
-    time.sleep(1)
-    print(f'Цикл выполнен, цена XRPUSDT составляет { current_price}')
+    time.sleep(60)
